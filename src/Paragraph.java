f108711207a71117649803536929f4bcd96f6cb3
gibihub <33116091+gibihub@users.noreply.github.com>
2019-11-04 17:07:27 +0200

    Lab 5
    LAb 5

--- src/Paragraph.java
+++ src/Paragraph.java
@@ -2,10 +2,25 @@
 public class Paragraph implements Element {
 
 	private String title;
+	private String text;
+	AlignStrategy align;
+	
+
+	public Paragraph(String title, String text) {
+		super();
+		this.title = title;
+		this.text = text;
+	}
 
 	public Paragraph(String title) {
 		super();
 		this.title = title;
+	}
+	
+	public void setAlignStrategy(AlignStrategy align)
+	{
+		this.align=align;
+		
 	}
 
 	public String getTitle() {
@@ -18,7 +33,15 @@ public class Paragraph implements Element {
 
 	public void print()
 	{
-		System.out.println("Paragraph: " + this.title);
+		System.out.print("Paragraph: " + this.title);
+		if(this.align!=null)
+		{
+			align.print(this.title);
+		}
+		else
+		{
+			System.out.println(this.title);
+		}
 	}
 	
 }
